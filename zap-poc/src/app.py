164d039a7c1e4495686be75dad2e60b97644f195
Ruturaj <ruturajgidde6460@gmail.com>
2025-10-24 17:30:25 +0530

    changes in app.py

--- zap-poc/src/app.py
+++ zap-poc/src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask app running on EKS!"
+    return "Hello from Flask app running on EKS 2285!"
 
 @app.route('/api/user')
 def user():
